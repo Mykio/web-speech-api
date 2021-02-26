5f67dad947ef258a3d921205c6e9144f4ef63a24
Mykio <adrien.mancini@gmail.com>
2021-02-26 18:08:31 +0100

    Constant Listening

--- speech-color-changer/script.js
+++ speech-color-changer/script.js
@@ -5,24 +5,69 @@ var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEv
 var colors = [ 'aqua' , 'azure' , 'beige', 'bisque', 'black', 'blue', 'brown', 'chocolate', 'coral', 'crimson', 'cyan', 'fuchsia', 'ghostwhite', 'gold', 'goldenrod', 'gray', 'green', 'indigo', 'ivory', 'khaki', 'lavender', 'lime', 'linen', 'magenta', 'maroon', 'moccasin', 'navy', 'olive', 'orange', 'orchid', 'peru', 'pink', 'plum', 'purple', 'red', 'salmon', 'sienna', 'silver', 'snow', 'tan', 'teal', 'thistle', 'tomato', 'turquoise', 'violet', 'white', 'yellow'];
 var grammar = '#JSGF V1.0; grammar colors; public <color> = ' + colors.join(' | ') + ' ;'
 
-var recognition = new SpeechRecognition();
 var speechRecognitionList = new SpeechGrammarList();
 speechRecognitionList.addFromString(grammar, 1);
-recognition.grammars = speechRecognitionList;
-recognition.continuous = false;
-recognition.lang = 'en-US';
-recognition.interimResults = false;
-recognition.maxAlternatives = 1;
+
+const initRecognition = () => {
+  var reco = new SpeechRecognition();
+  reco.grammars = speechRecognitionList;
+  reco.continuous = false;
+  reco.lang = 'en-US';
+  reco.interimResults = false;
+  reco.maxAlternatives = 1;
+
+  reco.onresult = function(event) {
+    color = event.results[0][0].transcript;
+    diagnostic.textContent = 'Result received: ' + color + '.';
+    bg.style.backgroundColor = color;
+    console.log(event.results[0][0].transcript);
+  }
+
+  reco.onstart = () => {
+    console.log("reco started")
+  }
+
+  reco.onend = () => {
+    console.log('reco ended')
+    //reco = initRecognition(reco)
+    //console.log('reco rewritten')
+    reco.start()
+  }
+
+
+  reco.onresult = function(event) {
+    // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
+    // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
+    // It has a getter so it can be accessed like an array
+    // The first [0] returns the SpeechRecognitionResult at the last position.
+    // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
+    // These also have getters so they can be accessed like arrays.
+    // The second [0] returns the SpeechRecognitionAlternative at position 0.
+    // We then return the transcript property of the SpeechRecognitionAlternative object
+    var color = event.results[0][0].transcript;
+    diagnostic.textContent = 'Result received: ' + color + '.';
+    bg.style.backgroundColor = color;
+    console.log(event.results[0][0].transcript);
+    reco.stop();
+  }
+
+  return reco;
+}
+
+var recognition = initRecognition();
 
 var diagnostic = document.querySelector('.output');
 var bg = document.querySelector('html');
 var hints = document.querySelector('.hints');
 
 var colorHTML= '';
+
 colors.forEach(function(v, i, a){
-  console.log(v, i);
+  //console.log(v, i);
   colorHTML += '<span style="background-color:' + v + ';"> ' + v + ' </span>';
 });
+
+
 hints.innerHTML = 'Tap/click then say a color to change the background color of the app. Try ' + colorHTML + '.';
 
 document.body.onclick = function() {
@@ -30,29 +75,6 @@ document.body.onclick = function() {
   console.log('Ready to receive a color command.');
 }
 
-recognition.onresult = function(event) {
-  // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
-  // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
-  // It has a getter so it can be accessed like an array
-  // The first [0] returns the SpeechRecognitionResult at the last position.
-  // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
-  // These also have getters so they can be accessed like arrays.
-  // The second [0] returns the SpeechRecognitionAlternative at position 0.
-  // We then return the transcript property of the SpeechRecognitionAlternative object
-  var color = event.results[0][0].transcript;
-  diagnostic.textContent = 'Result received: ' + color + '.';
-  bg.style.backgroundColor = color;
-  console.log('Confidence: ' + event.results[0][0].confidence);
-}
 
-recognition.onspeechend = function() {
-  recognition.stop();
-}
 
-recognition.onnomatch = function(event) {
-  diagnostic.textContent = "I didn't recognise that color.";
-}
 
-recognition.onerror = function(event) {
-  diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
-}
